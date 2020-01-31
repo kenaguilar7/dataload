0dd1f94bc4c48a5e59a338b9c1f6f0b1f531a58b
Kenneth <kenaguilar.steve@gmail.com>
2020-01-31 08:38:02 -0600

    Folders names

---
+++ Names.cs
@@ -0,0 +1,32 @@
+namespace DataLoad
+{
+    public enum Names {
+        PDT01,
+        PDT02,
+        PFT01,
+        PFT02,
+        PFT03,
+        PFT04,
+        PFT05,
+        PPT01,
+        PGT01,
+        PGT02,
+        PGT03,
+        PHT02,
+        PITC03,
+        PITC04,
+        PITC05,
+        PITC08,
+        PITC09,
+        PITC10,
+        PITC11,
+        PITC12,
+        PITC13,
+        PITC15,
+        PIT01,
+        PIT04,
+        PDTC01,
+        PDTC12
+
+    }
+}
